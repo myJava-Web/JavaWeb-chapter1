1d3134d149c2d14ea1e8428fdce4f58d95d66d66
leonardliuz <leonard.liuzz@sohu.com>
2016-12-07 21:56:17 +0800

    HelloServlet lost key-word 'public'.

--- src/main/java/com/tuji/chapter1/HelloServlet.java
+++ src/main/java/com/tuji/chapter1/HelloServlet.java
@@ -16,10 +16,10 @@ import java.util.Date;
 
 
 @WebServlet("/hello")
-class HelloServlet extends HttpServlet {
+public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        //super.doGet(req, resp);
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String currentTime = dateFormat.format(new Date());
         req.setAttribute("currentTime",currentTime);
